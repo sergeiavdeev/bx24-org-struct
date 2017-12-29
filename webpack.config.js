b1e018612e7bd2fde258a87715090f601e65b891
avdeev.sa <avdeev.sa@pecom.ru>
2017-12-29 12:10:26 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,104 @@
+var path = require('path')
+var webpack = require('webpack')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const ZipWebpackPlugin = require('zip-webpack-plugin')
+
+module.exports = {
+  entry: {
+    app: './src/main.js',
+    install: './src/install.js'
+  },
+  output: {
+    path: path.resolve(__dirname, './dist/js'),
+    publicPath: '/dist/js/',
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: ['.js', '.vue'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      'public': path.resolve(__dirname, './public')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+          }
+          // other vue-loader options go here
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.styl$/,
+        loader: ['style-loader', 'css-loader', 'stylus-loader']
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true
+  },
+  performance: {
+    hints: false
+  },
+  devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '';
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: 'index.html',
+        to: path.resolve(__dirname, './dist'),
+        transform: function (content, path) {
+          return content.toString().replace("/dist", "");
+        }
+      },
+      {
+        from: 'install.html',
+        to: path.resolve(__dirname, './dist'),
+        transform: function (content, path) {
+          return content.toString().replace("/dist", "");
+        }
+      }
+    ]),
+    new ZipWebpackPlugin({
+      path: path.resolve(__dirname, './dist'),
+      filename: 'bx24-org-struct.zip',
+      pathPrefix: './js',
+      include: [/\.(js|html)$/]
+    })
+  ])
+}
