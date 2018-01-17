aa639213b75d72d038734f606c2187abf0a77d9e
avdeev.sa <avdeev.sa@pecom.ru>
2018-01-17 16:54:20 +0300

    Animation add

--- src/Item.vue
+++ src/Item.vue
@@ -3,10 +3,12 @@
     <div @click="onclick" :class="item.children && item.children.length > 0 ? 'click' : ''">
       {{item.NAME}}
     </div>
-    <ul v-show="open">
+    <transition-group name="fade">
+    <ul v-show="open" :key="0">
       <item v-for="item in item.children" :item="item" :key="item.ID" v-on:add="onAdd"></item>
-      <li @click="add" :class="'click'">+</li>
+      <li @click="add" :class="'click'" :key="-1">+</li>
     </ul>
+    </transition-group>
   </li>
 </template>
 
@@ -41,6 +43,13 @@
     cursor: pointer;
     text-decoration: underline;
     user-select: none;
+  }
+
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .7s;
+  }
+  .fade-enter, .fade-leave-to /* .fade-leave-active до версии 2.1.8 */ {
+    opacity: 0;
   }
 
 </style>
